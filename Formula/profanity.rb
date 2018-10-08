62e86660197d64507f94c8c830de54af36fa9751
baileydog <baileydog@blend.com>
2018-10-07 23:39:38 -0700

    Brew formula update for profanity version v0.2.0

--- Formula/profanity.rb
+++ Formula/profanity.rb
@@ -1,11 +1,9 @@
 class Profanity < Formula
   desc "Apply profanity rules to a directory tree."
   homepage "https://github.com/blend/go-sdk/tree/master/cmd/profanity/README.md"
-  url "https://github.com/blend/go-sdk/releases/download/v0.1.0/profanity_0.1.0_darwin_amd64.tar.gz"
-  version "0.1.0"
-  sha256 "f46e41449deda1732806f1c1b26726ec72b63bbdb082ed96c84b24bceb2764c6"
-  
-  depends_on "go"
+  url "https://github.com/blend/go-sdk/releases/download/v0.2.0/profanity_0.2.0_darwin_amd64.tar.gz"
+  version "0.2.0"
+  sha256 "a48030c6ab3565725795a7794f4f27d417e18f403a4472b2e049fb57329c476f"
 
   def install
     bin.install "profanity"

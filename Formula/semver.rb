97dd11c1dbfdaae8764ed6e4c60fe1b41a8f50de
baileydog <baileydog@blend.com>
2018-10-18 18:26:09 -0700

    Brew formula update for semver version v0.3.1

--- Formula/semver.rb
+++ Formula/semver.rb
@@ -1,9 +1,9 @@
 class Semver < Formula
   desc "Semver management CLI helper."
   homepage "https://github.com/blend/go-sdk/tree/master/cmd/semver/README.md"
-  url "https://github.com/blend/go-sdk/releases/download/v0.3.0/semver_0.3.0_darwin_amd64.tar.gz"
-  version "0.3.0"
-  sha256 "bac1e28e4bb82315d5e25ffc2f5d67308b3c3c1a791e7f988f42ff6dc33e8877"
+  url "https://github.com/blend/go-sdk/releases/download/v0.3.1/semver_0.3.1_darwin_amd64.tar.gz"
+  version "0.3.1"
+  sha256 "4f6a6500d6b605928d2b9a38f9499bb7a879afdd972f4e7d6c861b00c659b9f6"
 
   def install
     bin.install "semver"

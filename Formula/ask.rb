7fca27557f28cf1b06056b44be0523e1cc092264
baileydog <baileydog@blend.com>
2018-10-31 13:08:12 -0700

    Brew formula update for ask version v0.3.3

--- Formula/ask.rb
+++ Formula/ask.rb
@@ -1,9 +1,9 @@
 class Ask < Formula
   desc "Prompt a commandline user for values for a given list of fields."
   homepage "https://github.com/blend/go-sdk/tree/master/cmd/ask/README.md"
-  url "https://github.com/blend/go-sdk/releases/download/v0.3.1/ask_0.3.1_darwin_amd64.tar.gz"
-  version "0.3.1"
-  sha256 "50283d715fb65775cd8660aba1b6a0e0589c80e976c51114deeee1ccb797c338"
+  url "https://github.com/blend/go-sdk/releases/download/v0.3.3/ask_0.3.3_darwin_amd64.tar.gz"
+  version "0.3.3"
+  sha256 "9cf21007d1533367353cb1c45fb8ad2a61fc774724cf2616d9e1b1335fc98925"
 
   def install
     bin.install "ask"

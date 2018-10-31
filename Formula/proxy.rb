065badea9298ab75dd11050966ce26f1fbd4ae98
baileydog <baileydog@blend.com>
2018-10-31 13:08:34 -0700

    Brew formula update for proxy version v0.3.3

--- Formula/proxy.rb
+++ Formula/proxy.rb
@@ -1,9 +1,9 @@
 class Proxy < Formula
   desc "A lightweight http reverse proxy."
   homepage "https://github.com/blend/go-sdk/tree/master/cmd/proxy/README.md"
-  url "https://github.com/blend/go-sdk/releases/download/v0.3.1/proxy_0.3.1_darwin_amd64.tar.gz"
-  version "0.3.1"
-  sha256 "0f2bb234143e8488eedfdda9be952d0e27295390f892c5a561b4013c22a62825"
+  url "https://github.com/blend/go-sdk/releases/download/v0.3.3/proxy_0.3.3_darwin_amd64.tar.gz"
+  version "0.3.3"
+  sha256 "e12caff0ccd609b45a4ea75c0325de1d76bb8a2b711472543c9eae854601f125"
 
   def install
     bin.install "proxy"

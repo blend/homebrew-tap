34743813adaa99ea4062b9e8bfc2112fb050e1d2
baileydog <baileydog@blend.com>
2018-10-31 13:08:37 -0700

    Brew formula update for recover version v0.3.3

--- Formula/recover.rb
+++ Formula/recover.rb
@@ -1,9 +1,9 @@
 class Recover < Formula
   desc "Lightweight process recovery."
   homepage "https://github.com/blend/go-sdk/tree/master/cmd/recover/README.md"
-  url "https://github.com/blend/go-sdk/releases/download/v0.3.1/recover_0.3.1_darwin_amd64.tar.gz"
-  version "0.3.1"
-  sha256 "6b42047db5f050bfa2452a2816aa7419b31ee3839a73f5ebd2d457f41eb32e14"
+  url "https://github.com/blend/go-sdk/releases/download/v0.3.3/recover_0.3.3_darwin_amd64.tar.gz"
+  version "0.3.3"
+  sha256 "e8596428a9b40bd730843eaf13312f7bd4d7a56c77e0c98380dd3e5506b0b1f9"
 
   def install
     bin.install "recover"

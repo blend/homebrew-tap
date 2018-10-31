274cfb3f02d6f9adca5d2fe1c751cc35fb5c064e
baileydog <baileydog@blend.com>
2018-10-31 13:20:21 -0700

    Brew formula update for coverage version v0.3.5

--- Formula/coverage.rb
+++ Formula/coverage.rb
@@ -1,9 +1,9 @@
 class Coverage < Formula
   desc "Coordinate coverage enforcement for golang repos."
   homepage "https://github.com/blend/go-sdk/tree/master/cmd/coverage/README.md"
-  url "https://github.com/blend/go-sdk/releases/download/v0.3.4/coverage_0.3.4_darwin_amd64.tar.gz"
-  version "0.3.4"
-  sha256 "1f813b78f917750d5a372ea810ed8a0e5e747f06f8de39a20da0fe205d429132"
+  url "https://github.com/blend/go-sdk/releases/download/v0.3.5/coverage_0.3.5_darwin_amd64.tar.gz"
+  version "0.3.5"
+  sha256 "56d163bda7ae0ff3e7d8760aecbd150cbd51c6adcc91679c2d08f1e7b677052c"
 
   def install
     bin.install "coverage"

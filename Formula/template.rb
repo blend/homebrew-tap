dd614137db73add36f4e97634f37c03856a120a0
baileydog <baileydog@blend.com>
2018-10-31 13:08:47 -0700

    Brew formula update for template version v0.3.3

--- Formula/template.rb
+++ Formula/template.rb
@@ -1,9 +1,9 @@
 class Template < Formula
   desc "A powerful CLI template generator based on golang's `text/template`"
   homepage "https://github.com/blend/go-sdk/tree/master/cmd/template/README.md"
-  url "https://github.com/blend/go-sdk/releases/download/v0.3.1/template_0.3.1_darwin_amd64.tar.gz"
-  version "0.3.1"
-  sha256 "6b5de1e8a3a9f05650d512e5415fa7777d724cb14d085c78cf881d31932800f8"
+  url "https://github.com/blend/go-sdk/releases/download/v0.3.3/template_0.3.3_darwin_amd64.tar.gz"
+  version "0.3.3"
+  sha256 "2259e28ccffbbba4168776e38b676607d7e80e92d98a633584c7e9cde9979986"
 
   def install
     bin.install "template"
